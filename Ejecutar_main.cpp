8ea8484e6d13729cf08a988bb6b66ccde8ef8903
miguef10 <miguef10hd@gmail.com>
2020-08-31 22:52:31 -0500

    creacion de celdas

--- Ejecutar_main.cpp
+++ Ejecutar_main.cpp
@@ -30,4 +30,25 @@ void cuadro(int x1,int y1,int x2, int y2)
 	gotoxy(x1,y2);cout<<char(200);
 	gotoxy(x2,y2);cout<<char(188);
 }
- 
+
+/*Creacion de celdas*/
+void celdas(int x1,int y1,int x2,int y2)
+{
+	int x;
+	int y;
+	for (x=x1;x<=x2;x++)
+	{
+		gotoxy (x,y1);cout<<char (205);
+		gotoxy (x,y2);cout<<char (205);
+	}
+	for (y=y1;y<=y2;y++)
+	{
+		gotoxy (x1,y);cout<<char (186);
+		gotoxy (x2,y);cout<<char (186);
+	}
+	gotoxy(x1,y1);cout<<char(201);
+	gotoxy(x2,y1);cout<<char(187);
+	gotoxy(x1,y2);cout<<char(200);
+	gotoxy(x2,y2);cout<<char(188);
+}
+
